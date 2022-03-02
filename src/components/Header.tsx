698b113ee80f7a8a2268391883e63b43a187f668
AlexandruColibnic <59502870+AlexandruColibnic@users.noreply.github.com>
2022-03-02 14:06:10 +0200

    Commit number 2

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Header = () => {
+    return <header className='header'>
+        <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
+    </header>
+}
+
